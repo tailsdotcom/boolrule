9f97b47606b26c73fcdfa85b9ee0fb240953ceac
Steve Webster <spjwebster@gmail.com>
2016-09-30 16:56:12 +0100

    Fix repo URL in travis setup script

--- travis_pypi_setup.py
+++ travis_pypi_setup.py
@@ -21,7 +21,7 @@ except:
     from urllib.request import urlopen
 
 
-GITHUB_REPO = 'spjwebster/boolrule'
+GITHUB_REPO = 'tailsdotcom/boolrule'
 TRAVIS_CONFIG_FILE = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), '.travis.yml')
 
