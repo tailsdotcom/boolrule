50c1069c912ec0e6bf7ba22dcad4eea52b0d33cd
Steve Webster <spjwebster@gmail.com>
2016-09-30 21:58:50 +0100

    Bump version: 0.1.1 → 0.1.2

--- boolrule/__init__.py
+++ boolrule/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = 'Steve Webster'
 __email__ = 'spjwebster@gmail.com'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 from .boolrule import BoolRule, MissingVariableException, UnknownOperatorException  # noqa
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='boolrule',
-    version='0.1.1',
+    version='0.1.2',
     description="Simple boolean expression evaluation engine",
     long_description=readme + '\n\n' + history,
     author="Steve Webster",
