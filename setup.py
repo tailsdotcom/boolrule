1f440c17aaa1da1f6abc384526dc04cb00e83b42
Steve Webster <spjwebster@gmail.com>
2018-01-15 08:23:58 +0000

    Bump version: 0.2.1 → 0.3.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='boolrule',
-    version='0.2.1',
+    version='0.3.0',
     description="Simple boolean expression evaluation engine",
     long_description=readme + '\n\n' + history,
     author="Steve Webster",
