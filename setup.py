9d22889465c75a0b90144462d07914a654869c38
Steve Webster <spjwebster@Steves-MacBook-Pro.local>
2020-09-09 10:59:17 +0100

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='boolrule',
-    version='0.3.0',
+    version='0.3.1',
     description="Simple boolean expression evaluation engine",
     long_description=readme + '\n\n' + history,
     author="Steve Webster",
